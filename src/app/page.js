538f6219137722df08ff072d42971f421e44e595
arturoaguileraa <arturoaguileraglez@gmail.com>
2024-12-12 00:16:18 +0100

    Modify overflow and margin

--- src/app/page.js
+++ src/app/page.js
@@ -8,9 +8,9 @@ import AchievementsSection from "./components/AchievementsSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
+    <main className="flex min-h-screen flex-col bg-[#121212] overflow-hidden">
       <Navbar />
-      <div className="container mt-24 mx-auto px-12 py-4">
+      <div className="container mt-14 mx-auto px-6 py-4 overflow-hidden">
         <HeroSection />
         {/* <AchievementsSection /> */}
         <AboutSection />
