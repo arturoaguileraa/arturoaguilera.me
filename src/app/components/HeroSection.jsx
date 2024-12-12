a9830a2483a00e46f5b9699f5c96c409ed2af08e
arturoaguileraa <arturoaguileraglez@gmail.com>
2024-12-12 14:05:31 +0100

    Fix responsive

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 const HeroSection = () => {
   return (
     <section className="lg:py-16">
-      <div className="grid grid-cols-1 sm:grid-cols-12 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-12 gap-6 sm:m-8 md:m-2">
         {/* Imagen: Orden invertido en pantallas pequeñas */}
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
@@ -16,13 +16,13 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-12 sm:col-span-4 order-1 sm:order-2 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative overflow-hidden">
+          <div className="rounded-full bg-[trasparent] w-[250px] h-[250px] lg:w-[300px] lg:h-[300px] relative overflow-hidden border border-[#33353F]">
             <Image
               src="/images/profilephoto.png"
               alt="My profile photo"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 object-cover"
-              width={400}
-              height={400}
+              width={300}
+              height={300}
             />
           </div>
         </motion.div>
@@ -32,9 +32,9 @@ const HeroSection = () => {
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-          className="col-span-12 sm:col-span-8 order-2 sm:order-1 place-self-center text-center sm:text-left"
+          className="col-span-12 sm:col-span-8 order-2 sm:order-1 place-self-left text-center sm:text-left sm:mt-8 md:mt-8 lg:ml-16"
         >
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+          <h1 className="text-white mb-4 text-4xl sm:text-3xl lg:text-6xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
               Hello, I&apos;m
             </span>
@@ -66,7 +66,7 @@ const HeroSection = () => {
               href="/#contact"
               className="px-6 inline-block py-3 w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
-              Hire Me
+              Let's talk!
             </Link>
             <Link
               href="/cv.pdf"
