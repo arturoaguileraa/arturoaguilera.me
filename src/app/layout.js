d5e91a5c8161ff96a4d9028da0334573a7128d43
arturoaguileraa <arturoaguileraglez@gmail.com>
2024-12-13 00:29:40 +0100

    Modify openGraph metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,12 +4,12 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Arturo Aguilera González - Portfolio',
-  description: 'Portfolio of Arturo Aguilera González. Malware analysis, artificial intelligence, and web development with a minimalist and functional approach. Explore my projects and skills.',
+  title: 'Arturo Aguilera González',
+  description: 'Portfolio of Arturo Aguilera González. Web Development, Artificial Intelligence, and Malware Analysis. Explore my projects and skills.',
   openGraph: {
     url: "https://arturoaguilera.es",
-    siteName: "Buche - Tu app de gestión de pedidos",
-    description: "Gestiona fácilmente tus pedidos con Buche, la mejor herramienta para establecimientos.",
+    siteName: "Arturo Aguilera González",
+    description: 'Portfolio of Arturo Aguilera González. Web Development, Artificial Intelligence, and Malware Analysis. Explore my projects and skills.',
     images: [{ url: "https://arturoaguilera.es/projects/portfolio.jpg" }]
   },
 }
