1083ff874d4d201f8ae574f0e2f45cf580bbc948
arturoaguileraa <arturoaguileraglez@gmail.com>
2024-12-13 00:12:00 +0100

    Change about section

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -60,12 +60,9 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
-            interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
+            I am a Master&apos;s student in Artificial Intelligence with a background in Computer Science and Mathematics.
+            Experienced in web development with technologies like React, NestJS, and PostgreSQL, and currently training in malware analysis.
+            I am passionate about learning new technologies and always looking to further develop professionally.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
