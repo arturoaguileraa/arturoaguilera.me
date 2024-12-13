791c48b8d60ab0c9a73df0f3c1889666c0aa31ca
arturoaguileraa <arturoaguileraglez@gmail.com>
2024-12-13 01:23:58 +0100

    Modify About section

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -9,13 +9,14 @@ const TAB_DATA = [
     id: "skills",
     content: (
       <ul className="list-disc pl-2">
-        <li>Node.js</li>
-        <li>Express</li>
-        <li>PostgreSQL</li>
-        <li>TypeScript</li>
-        <li>JavaScript</li>
         <li>React</li>
         <li>NestJS</li>
+        <li>TypeScript</li>
+        <li>Python</li>
+        <li>Express</li>
+        <li>PostgreSQL</li>
+        <li>JavaScript</li>
+        <li>...</li>
       </ul>
     ),
   },
@@ -24,10 +25,15 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>University Expert Diploma in Reverse Engineering and Malware
-          Intelligence - GSEC, Málaga</li>
-        <li>Artificial Intelligence - Universidad Internacional de La Rioja (UNIR), Online</li>
-        <li>Mathematic & Computer Science - University of Malaga, Spain.</li>
+        <li>
+          Artificial Intelligence - Universidad Internacional de La Rioja
+          (UNIR), Online
+        </li>
+        <li>
+          University Expert Diploma in Reverse Engineering and Malware
+          Intelligence - GSEC, Málaga
+        </li>
+        <li>Mathematic & Computer Science - University of Málaga, Málaga</li>
       </ul>
     ),
   },
@@ -36,8 +42,29 @@ const TAB_DATA = [
     id: "certifications",
     content: (
       <ul className="list-disc pl-2">
-        <li>Cambridge B2 First Title</li>
-        <li>Backend Development and APIs Bootcamp - freeCodeCamp</li>
+        <li>
+          <a
+            href="https://verificationservice.cambridgeenglish.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block ml-2 text-blue-400 hover:underline"
+          >
+            Cambridge B2 First Title 🇬🇧 🇺🇸
+          </a>
+          {" "}(REF: 187ES0650049)
+        </li>
+
+        <li>
+
+          <a
+            href="https://www.freecodecamp.org/certification/arturoaguilera/back-end-development-and-apis"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block ml-2 text-blue-400 hover:underline"
+          >
+            Backend Development and APIs Bootcamp - freeCodeCamp 🔗
+          </a>
+        </li>
       </ul>
     ),
   },
@@ -60,9 +87,12 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a Master&apos;s student in Artificial Intelligence with a background in Computer Science and Mathematics.
-            Experienced in web development with technologies like React, NestJS, and PostgreSQL, and currently training in malware analysis.
-            I am passionate about learning new technologies and always looking to further develop professionally.
+            I am a Master&apos;s student in Artificial Intelligence with a
+            background in Computer Science and Mathematics. Experienced in web
+            development with technologies like React, NestJS, and PostgreSQL,
+            and currently training in malware analysis. I am passionate about
+            learning new technologies and always looking to further develop
+            professionally.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
